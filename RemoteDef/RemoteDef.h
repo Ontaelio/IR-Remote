b7543ebfe2754a1605cc65fa51c8369ed69c14a2
Ontaelio <Ontaelio@users.noreply.github.com>
2018-11-20 00:17:41 +0300

    Add files via upload

---
+++ RemoteDef/RemoteDef.h
@@ -0,0 +1,135 @@
+//LG TV remote buttons
+#define TVenergy 0x20DFA956
+#define TVpower 0x20DF10EF
+#define TVtvrad 0x20DF0FF0
+
+#define TVguide 0x20DFD52A
+#define TVinfo 0x20DF55AA
+#define TVratio 0x20DF9E61
+#define TVinput 0x20DFD02F
+
+#define TV1 0x20DF8877 //551520375
+#define TV2 0x20DF48B7
+#define TV3 0x20DFC837
+#define TV4 0x20DF28D7
+#define TV5 0x20DFA857
+#define TV6 0x20DF6897
+#define TV7 0x20DFE817
+#define TV8 0x20DF18E7
+#define TV9 0x20DF9867
+#define TV0 0x20DF08F7
+#define TVlist 0x20DFCA35
+#define TVqview 0x20DFA25D
+
+#define TVsettings 0x20DFC23D
+#define TVqmenu 0x20DFA25D
+#define TVback 0x20DF14EB
+#define TVexit 0x20DFDA25
+#define TVOK 0x20DF22DD
+
+#define TVup 0x20DF02FD
+#define TVright 0x20DF609F
+#define TVdown 0x20DF827D
+#define TVleft 0x20DFE01F
+
+#define TVvolup 0x20DF40BF //551502015
+#define TVvoldown 0x20DFC03F //551534655
+#define TVfav 0x20DF7887
+#define TVad 0x20DF8976
+#define TVmute 0x20DF906F //551522415
+#define TVpageup 0x20DF00FF
+#define TVpagedown 0x20DF807F
+
+#define TVred 0x20DF4EB1 //551505585
+#define TVgreen 0x20DF8E71 //551521905
+#define TVyellow 0x20DFC639 //551536185
+#define TVblue 0x20DF8679 //551519865
+
+#define TVstop 0x20DF8D72
+#define TVplay 0x20DF0DF2
+#define TVpause 0x20DF5DA2
+#define TVsubtitle 0x20DF9C63
+#define TVfastback 0x20DFF10E
+#define TVfastforward 0x20DF718E
+#define TVtext 0x20DF04FB
+#define TVtopt 0x20DF847B
+
+#define TVrepeat 0xFFFFFFFF
+
+//RGB remote for Chinese RGB strip
+
+#define RGBremoteLightUp 0xFF3AC5
+#define RGBremoteLightDown 0xFFBA45
+#define RGBremotePlay 0xFF827D
+#define RGBremotePower 0xFF02FD
+
+#define RGBremoteR0 0xFF1AE5
+#define RGBremoteR1 0xFF2AD5
+#define RGBremoteR2 0xFF0AF5
+#define RGBremoteR3 0xFF38C7
+#define RGBremoteR4 0xFF18E7
+
+#define RGBremoteG0 0xFF9A65
+#define RGBremoteG1 0xFFAA55
+#define RGBremoteG2 0xFF8A75
+#define RGBremoteG3 0xFFB847
+#define RGBremoteG4 0xFF9867
+
+#define RGBremoteB0 0xFFA25D
+#define RGBremoteB1 0xFF926D
+#define RGBremoteB2 0xFFB24D
+#define RGBremoteB3 0xFF7887
+#define RGBremoteB4 0xFF58A7
+
+#define RGBremoteW0 0xFF22DD
+#define RGBremoteW1 0xFF12ED
+#define RGBremoteW2 0xFF32CD
+#define RGBremoteW3 0xFFF807
+#define RGBremoteW4 0xFFD827
+
+#define RGBremoteRup 0xFF28D7
+#define RGBremoteRdown 0xFF08F7
+#define RGBremoteGup 0xFFA857
+#define RGBremoteGdown 0xFF8877
+#define RGBremoteBup 0xFF6897
+#define RGBremoteBdown 0xFF48B7
+
+#define RGBremoteDIY1 0xFF30CF
+#define RGBremoteDIY2 0xFFB04F
+#define RGBremoteDIY3 0xFF708F
+#define RGBremoteDIY4 0xFF10EF
+#define RGBremoteDIY5 0xFF906F
+#define RGBremoteDIY6 0xFF50AF
+
+#define RGBremoteQuick 0xFFE817
+#define RGBremoteSlow 0xFFC837
+#define RGBremoteAuto 0xFFF00F
+#define RGBremoteFlash 0xFFD02F
+#define RGBremoteJump3 0xFF20DF
+#define RGBremoteJump7 0xFFA05F
+#define RGBremoteFade3 0xFF609F
+#define RGBremoteFade7 0xFFE01F
+
+// chinese remote CarMP3
+#define CarMP3CHminus 0xFFA25D
+#define CarMP3CH 0xFF629D
+#define CarMP3CHplus 0xFFE21D
+#define CarMP3prev 0xFF22DD
+#define CarMP3next 0xFF02FD
+#define CarMP3pp 0xFFC23D
+#define CarMP3volMinus 0xFFE01F
+#define CarMP3volPlus 0xFFA857
+#define CarMP3EQ 0xFF906F
+#define CarMP30 0xFF6897
+#define CarMP3100 0xFF9867
+#define CarMP3200 0xFFB04F
+#define CarMP31 0xFF30CF
+#define CarMP32 0xFF18E7
+#define CarMP33 0xFF7A85
+#define CarMP34 0xFF10EF
+#define CarMP35 0xFF38C7
+#define CarMP36 0xFF5AA5
+#define CarMP37 0xFF42BD
+#define CarMP38 0xFF4AB5
+#define CarMP39 0xFF52AD
+
